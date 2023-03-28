c5fcf7b6fa8107368aca0e8bffa2dddc63f1d77e
HasSanK911 <kaleemikennels@gmail.com>
2023-03-28 10:43:29 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,49 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtT20v8SPiWl3ShYtaqs2OqNwg1NOnZAM",
+  authDomain: "myproject-cef79.firebaseapp.com",
+  projectId: "myproject-cef79",
+  storageBucket: "myproject-cef79.appspot.com",
+  messagingSenderId: "753936452618",
+  appId: "1:753936452618:web:bf743b3fdc020cfdece3f3"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot(),
+    ReactiveFormsModule,
+    NgxPaginationModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
